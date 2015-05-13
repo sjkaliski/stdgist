aafcf3a03ae8cd76d8d7eecd4f7363960d1d613a
Larz Conwell <larzconwell@gmail.com>
2015-05-13 17:32:21 -0400

    Add file name support.
    Also clean up to better match stdslack code.

--- stdgist.go
+++ stdgist.go
@@ -1,19 +1,31 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
 )
 
 var (
 	gistsApiEndpoint = "https://api.github.com/gists"
+	fileName         = fmt.Sprintf("stdgist-%d", time.Now().Unix())
 )
+
+const usage = `Standard Gist
+
+Usage: stdgist [options]
+
+stdgist reads from standard input and posts the given
+input as a gist.
+
+Options:
+  --name, -n  Name for the file.
+`
 
 type gistReqBody struct {
 	Public bool                         `json:"public"`
@@ -24,33 +36,57 @@ type gistResBody struct {
 	HtmlUrl string `json:"html_url"`
 }
 
+func init() {
+	flag.StringVar(&fileName, "name", fileName, "")
+	flag.StringVar(&fileName, "n", fileName, "")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, usage)
+	}
+}
+
 func main() {
-	var content string
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		content += scanner.Text() + "\n"
+	flag.Parse()
+
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
-	var data bytes.Buffer
-	fileName := fmt.Sprintf("stdgist-%s", strconv.FormatInt(time.Now().Unix(), 10))
+	// Check if stdin is from a terminal (i.e. no input to read).
+	if stat.Mode()&os.ModeCharDevice != 0 {
+		fmt.Fprintln(os.Stderr, "Content needs to be given to stdin to use")
+		os.Exit(1)
+	}
+
+	var content bytes.Buffer
+	_, err = io.Copy(&content, os.Stdin)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 	body := gistReqBody{
 		Public: true,
 		Files: map[string]map[string]string{
 			fileName: map[string]string{
-				"content": content,
+				"content": content.String(),
 			},
 		},
 	}
 
-	encoder := json.NewEncoder(&data)
-	err := encoder.Encode(body)
+	content.Reset()
+	encoder := json.NewEncoder(&content)
+	err = encoder.Encode(body)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
-	res, err := http.Post(gistsApiEndpoint, "application/json", &data)
+	res, err := http.Post(gistsApiEndpoint, "application/json", &content)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 	defer res.Body.Close()
 
@@ -58,7 +94,8 @@ func main() {
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(&resBody)
 	if err != nil {
-		panic(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 
 	if resBody.HtmlUrl != "" {
